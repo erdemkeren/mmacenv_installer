4a1607c73d1c33b0ca9f37c41606c40baf234ef6
erdemkeren <erdemkeren@gmail.com>
2018-12-12 11:11:09 +0300

    feat: Adds alfred

---
+++ scripts/install_alfred.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+source scripts/helpers.sh
+
+function install_alfred() {
+  ## Define the name of the tool being installed
+  toolName="alfred"
+
+  installationStarted $toolName
+
+  wget -P_tmp_ https://cachefly.alfredapp.com/Alfred_3.7_938.dmg
+  install_dmg _tmp_/Alfred_3.7_938.dmg
+
+  open alfred://customsearch/Tureng/tureng/utf8/nospace/http%3A%2F%2Ftureng.com%2Ftr%2Fturkce-ingilizce%2F%7Bquery%7D
+  applicationConfigured $toolName
+
+  installationSucceed $toolName
+}
