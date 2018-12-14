88887362ad67ac2529264dafbe60a1674c9c4420
erdemkeren <erdemkeren@gmail.com>
2018-12-14 09:43:34 +0300

    feat: Adds github custom search

--- scripts/install_alfred.sh
+++ scripts/install_alfred.sh
@@ -11,7 +11,13 @@ function install_alfred() {
 #  wget -P_tmp_ https://cachefly.alfredapp.com/Alfred_3.7_938.dmg
 #  install_dmg _tmp_/Alfred_3.7_938.dmg
 
+  ## Tureng Search
   open alfred://customsearch/Tureng/tureng/utf8/nospace/http%3A%2F%2Ftureng.com%2Ftr%2Fturkce-ingilizce%2F%7Bquery%7D
+
+  ## Github Search
+  open alfred://customsearch/Github/github/utf8/nospace/https%3A%2F%2Fgithub.com%2Fsearch%3Fq%3D%7Bquery%7D
+  open alfred://customsearch/Github%20Profile%20or%20Project/githubp/utf8/nospace/https%3A%2F%2Fgithub.com%2F%7Bquery%7D
+  
   applicationConfigured $toolName
 
   installationSucceed $toolName
